8bca672bdde7285d59c98594c717f77b10881d27
sh111-coder <ohk9134@naver.com>
2024-03-08 15:34:38 +0900

    fix: MemberInfo PK 전략 IDENTITY로 fix

--- board-service/src/main/java/com/example/boardservice/board/domain/MemberInfo.java
+++ board-service/src/main/java/com/example/boardservice/board/domain/MemberInfo.java
@@ -2,6 +2,7 @@ package com.example.boardservice.board.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberInfo {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nickname;
