01f53fcf48f7e19622e2f066feefdaabacfe2a0d
sh111-coder <ohk9134@naver.com>
2024-03-08 17:45:48 +0900

    test: OpenFeignClient @AutoConfigureWireMock 사용하도록 리팩토링

--- shboard-common/src/testFixtures/java/com/example/common/OpenFeignClientTest.java
+++ shboard-common/src/testFixtures/java/com/example/common/OpenFeignClientTest.java
@@ -1,33 +1,25 @@
 package com.example.common;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.TestPropertySource;
 
-// @AutoConfigureWireMock(port = 0)
+@AutoConfigureWireMock(port = 0)
 @TestPropertySource(properties = {
         "member.find.feign-endpoint=http://localhost:${wiremock.server.port}"
 })
 public abstract class OpenFeignClientTest {
 
-    private static final int RANDOM_PORT = 0;
-
-    /**
-     * @Autowired
-     * private WireMockServer wireMockServer;
-     */
-    private static WireMockServer wireMockServer = new WireMockServer(RANDOM_PORT);
+    @Autowired
+    private WireMockServer wireMockServer;
 
     @BeforeEach
     void setUp() {
-        wireMockServer.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
         wireMockServer.stop();
+        wireMockServer.start();
     }
 
     @AfterEach
